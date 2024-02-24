14b8b0bcda9131eca4e8137a9ef3155f627cc1df
red <red@dreamleaves.org>
2024-02-24 21:54:27 +0100

    Chore: Testing the CLI

---
+++ tests/cli.rs
@@ -0,0 +1,42 @@
+use assert_cmd::Command;
+
+#[test]
+fn invalid_year_past() -> Result<(), Box<dyn std::error::Error>> {
+  let mut cmd = Command::cargo_bin("advent-rs")?;
+  cmd.arg("--year").arg("2014");
+  cmd.arg("--day").arg("01");
+  cmd.assert().failure().stderr(predicates::str::contains(
+    "advent-rs: Not implemented (Year 2014 Day 01v1)",
+  ));
+
+  Ok(())
+}
+
+#[test]
+fn year_2015_day_01_v1() -> Result<(), Box<dyn std::error::Error>> {
+  let mut cmd = Command::cargo_bin("advent-rs")?;
+  cmd.arg("--year").arg("2015");
+  cmd.arg("--day").arg("01");
+  cmd.arg("inputs/year_2015_day_01_input");
+  cmd
+    .assert()
+    .success()
+    .stdout(predicates::str::contains("Result: 138"));
+
+  Ok(())
+}
+
+#[test]
+fn year_2015_day_01_v2() -> Result<(), Box<dyn std::error::Error>> {
+  let mut cmd = Command::cargo_bin("advent-rs")?;
+  cmd.arg("--year").arg("2015");
+  cmd.arg("--day").arg("01");
+  cmd.arg("--version").arg("2");
+  cmd.arg("inputs/year_2015_day_01_input");
+  cmd
+    .assert()
+    .success()
+    .stdout(predicates::str::contains("Result: 1771"));
+
+  Ok(())
+}
