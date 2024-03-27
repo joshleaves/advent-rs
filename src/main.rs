721a9d65454bf661b27fce1b3c6dc9908378c50c
red <red@dreamleaves.org>
2024-03-27 20:22:42 +0100

    Chore: No need for common.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use std::fs;
+use std::io;
+use std::path::Path;
 use std::path::PathBuf;
 
 /// Solver for advent of code exercises
@@ -22,11 +25,31 @@ struct Args {
   input: Option<PathBuf>,
 }
 
-mod common;
+/// Returns a `String` input to use with a test.
+///
+/// If no argument is provided, the input will be read from STDIN.
+///
+/// # Arguments
+/// * `file_path` - File input to read from.
+pub fn fetch_input(file_path: Option<impl AsRef<Path>>) -> Result<String, std::io::Error> {
+  match file_path {
+    Some(filename) => fs::read_to_string(filename),
+    None => io::read_to_string(io::stdin()),
+  }
+}
 
+/// Software entry point
+///
+/// The arguments are taken from the command line inputs, parsed by Clap.
+///
+/// Arguments
+/// * `year` - The year of the exercise, from 2015 to today.
+/// * `day` - The day of the exercise, from 1 to 25.
+/// * `part` -  The part of the exercise, 1 or 2.
+/// * `input` - Input file path (will read from STDIN if empty).
 fn main() {
   let args = Args::parse();
-  let input: String = match common::fetch_input(args.input) {
+  let input: String = match fetch_input(args.input) {
     Ok(input_data) => input_data,
     Err(error) => {
       eprintln!("advent-rs: {}", error);
@@ -43,4 +66,18 @@ fn main() {
       std::process::exit(1)
     }
   };
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use std::io::ErrorKind;
+  use std::path::PathBuf;
+
+  #[test]
+  fn fetch_input_from_inexisting_file() {
+    let path: Option<PathBuf> = Some("foo.txt".into());
+    let error = fetch_input(path).unwrap_err();
+    assert_eq!(error.kind(), ErrorKind::NotFound);
+  }
 }
